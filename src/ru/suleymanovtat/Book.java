f2d1c7ff0d6c9e22611cc3e0a214f5649f635bdb
suleymanovtat <suleymanovtat@gmail.com>
2020-01-07 23:14:17 +0300

    created constructor

--- src/ru/suleymanovtat/Book.java
+++ src/ru/suleymanovtat/Book.java
@@ -4,4 +4,10 @@ class Book {
     String title;
     int yearOfPublishing;
     String[] authors;
+
+    public Book(String title, int yearOfPublishing, String[] authors) {
+        this.title = title;
+        this.yearOfPublishing = yearOfPublishing;
+        this.authors = authors;
+    }
 }
