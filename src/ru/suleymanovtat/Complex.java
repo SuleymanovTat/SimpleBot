3b28d534288f2246c9b3911d0f61c4eecc4f6193
suleymanovtat <suleymanovtat@gmail.com>
2020-01-08 23:41:15 +0300

    Created get set Complex class

--- src/ru/suleymanovtat/Complex.java
+++ src/ru/suleymanovtat/Complex.java
@@ -3,4 +3,20 @@ package ru.suleymanovtat;
 class Complex {
     double real;
     double image;
+
+    public double getReal() {
+        return real;
+    }
+
+    public void setReal(double real) {
+        this.real = real;
+    }
+
+    public double getImage() {
+        return image;
+    }
+
+    public void setImage(double image) {
+        this.image = image;
+    }
 }
