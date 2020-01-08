cc9b084709b7e938a2669cbb544151e323b9fc02
suleymanovtat <suleymanovtat@gmail.com>
2020-01-08 23:59:33 +0300

    Getting the fullname

---
+++ src/ru/suleymanovtat/User.java
@@ -0,0 +1,27 @@
+package ru.suleymanovtat;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+
+    public User() {
+        this.firstName = "";
+        this.lastName = "";
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        String first = firstName == null ? "" : firstName + " ";
+        String last = lastName == null ? "" : lastName + " ";
+        String fullName = first + last;
+        return fullName.isEmpty() ? "Unknown" : fullName;
+    }
+}
