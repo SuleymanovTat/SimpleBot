8ea4f41cf7966e6a2b169c578298753f0b9392c6
suleymanovtat <suleymanovtat@gmail.com>
2020-01-12 23:12:01 +0300

    Inheritance → Inheriting members 3

---
+++ src/ru/suleymanovtat/DerivedClass.java
@@ -0,0 +1,11 @@
+package ru.suleymanovtat;
+
+import ru.suleymanovtat.base.BaseClass;
+
+public class DerivedClass extends BaseClass {
+    private int b;
+
+    public int sum() {
+        return a + b;
+    }
+}
---
+++ src/ru/suleymanovtat/base/BaseClass.java
@@ -0,0 +1,5 @@
+package ru.suleymanovtat.base;
+
+public class BaseClass {
+    protected int a;
+}
