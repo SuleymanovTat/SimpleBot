b6ba89b82abffdca73bcae212ce8fc22e678b998
suleymanovtat <suleymanovtat@gmail.com>
2020-01-13 22:35:38 +0300

    Find incorrect assignments

--- src/ru/suleymanovtat/types/Main.java
+++ src/ru/suleymanovtat/types/Main.java
@@ -7,5 +7,16 @@ public class Main {
 //        D variable3 = new С();
         D variable4 = new D();
         D variable5 = new E();
+
+
+        A a1 = new A(); // (1)
+        A a2 = new С(); // (2)
+
+        B b1 = new B(); // (3)
+//        B b2 = new A(); // (4)
+        B b3 = new С(); // (5)
+
+//        С c1 = new B(); // (6)
+        С c2 = new С(); // (7)
     }
 }
