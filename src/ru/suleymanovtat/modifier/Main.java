276410b1a6e2caf4a6999b78fe2a90ece4a10468
suleymanovtat <suleymanovtat@gmail.com>
2020-01-13 21:45:57 +0300

    Protected modifier → Guess the output

---
+++ src/ru/suleymanovtat/modifier/Main.java
@@ -0,0 +1,10 @@
+package ru.suleymanovtat.modifier;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        System.out.println(counter.i);
+    }
+
+}
