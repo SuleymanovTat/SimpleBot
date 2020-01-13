22d67800a3f12eb14a7cd1c1a650d7572640a92b
suleymanovtat <suleymanovtat@gmail.com>
2020-01-13 17:23:15 +0300

    The keyword super → Employees

---
+++ src/ru/suleymanovtat/employeses/Employee.java
@@ -0,0 +1,26 @@
+package ru.suleymanovtat.employeses;
+
+class Employee {
+    String name;
+    String email;
+    int experience;
+
+    public Employee(String name, String email, int experience) {
+        this.name = name;
+        this.email = email;
+        this.experience = experience;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+}
+
