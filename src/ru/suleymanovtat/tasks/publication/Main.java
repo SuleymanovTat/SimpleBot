18ed060bfbe2262a810f00f1ebf2729621df7dd8
suleymanovtat <suleymanovtat@gmail.com>
2020-01-14 23:38:02 +0300

    Publication and its subclasses

--- src/ru/suleymanovtat/tasks/publication/Main.java
+++ src/ru/suleymanovtat/tasks/publication/Main.java
@@ -4,14 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Publication mPublication = new Publication("The new era");
         System.out.println(mPublication.getInfo());
-
-
         Newspaper mNewspaper = new Newspaper("Football results", "Sport news");
         System.out.println(mNewspaper.getInfo());
-
         Article mArticle = new Article("Why the Sun is hot", " Dr James Smith");
         System.out.println(mArticle.getInfo());
-
         Announcement mAnnouncement = new Announcement("Will sell a house", 30);
         System.out.println(mAnnouncement.getInfo());
     }
