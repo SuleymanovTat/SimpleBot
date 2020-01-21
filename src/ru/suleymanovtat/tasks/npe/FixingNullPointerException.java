ce0410aa0df3bbcea5b6b3e4b5c585afe3bef572
suleymanovtat <suleymanovtat@gmail.com>
2020-01-21 15:08:03 +0300

    toPrimitive

--- src/ru/suleymanovtat/tasks/npe/FixingNullPointerException.java
+++ src/ru/suleymanovtat/tasks/npe/FixingNullPointerException.java
@@ -10,5 +10,16 @@ class FixingNullPointerException {
         string = "null".equals(string) ? null : string;
         /* Do not change code above */
         System.out.println(string == null ? "NPE!" : string.toLowerCase());
+        System.out.println(toPrimitive(new Boolean("true")));
+        System.out.println(toPrimitive(new Boolean("false")));
+        System.out.println(toPrimitive(null));
+    }
+
+    public static boolean toPrimitive(Boolean b) {
+        if (b == null) {
+            return false;
+        } else {
+            return b;
+        }
     }
 }
